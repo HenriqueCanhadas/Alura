f22fd54c554daa548bb6cf6d21816dabea6b6c12
Henrique Canhadas <pedrosacanhadas@gmail.com>
2025-04-03 16:16:40 -0300

    att curso pandas

--- Curso Pandas Dados Limpeza/tarefa1.py
+++ Curso Pandas Dados Limpeza/tarefa1.py
@@ -65,4 +65,53 @@ dados_sem_vazio['cliente.tempo_servico'].fillna(
     ),inplace=True
 )
 
-print(dados_sem_vazio[filtro][['cliente.tempo_servico','conta.cobranca.mensal','conta.cobranca.Total']])
+dados_sem_vazio[filtro][['cliente.tempo_servico','conta.cobranca.mensal','conta.cobranca.Total']]
+
+
+dados_sem_vazio['conta.contrato'].value_counts()
+
+colunas_dropar = ['conta.contrato', 'conta.faturamente_eletronico', 'conta.metodo_pagamento']
+
+print(dados_sem_vazio[colunas_dropar].isna().any(axis=1).sum())
+
+df_sem_nulo = dados_sem_vazio.dropna(subset=colunas_dropar).copy()
+
+df_sem_nulo.reset_index(drop=True, inplace=True)
+
+df_sem_nulo.isna().sum()
+
+df_sem_nulo.describe()
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.boxplot(x=df_sem_nulo['cliente.tempo_servico'])
+#plt.show()
+
+q1 = df_sem_nulo['cliente.tempo_servico'].quantile(.25)
+q3 = df_sem_nulo['cliente.tempo_servico'].quantile(.75)
+iqr = q3-q1
+
+limite_inferior = q1 - 1.5*iqr
+limite_superior = q3 + 1.5*iqr
+
+print(q1)
+print(q3)
+print(iqr)
+print(limite_inferior)
+print(limite_superior)
+
+outliers_index = (df_sem_nulo['cliente.tempo_servico'] < limite_inferior) | (df_sem_nulo['cliente.tempo_servico'] > limite_superior)
+
+df_sem_nulo[outliers_index]['cliente.tempo_servico']
+
+df_sem_out = df_sem_nulo.copy()
+
+df_sem_out.loc[outliers_index, 'cliente.tempo_servico'] = np.ceil(
+        df_sem_out.loc[outliers_index, 'conta.cobranca.Total'] /
+        df_sem_out.loc[outliers_index, 'conta.cobranca.mensal'] 
+)
+
+sns.boxplot(x=df_sem_out['cliente.tempo_servico'])
+
+print(df_sem_out [outliers_index][['cliente.tempo_servico', 'conta.cobranca.mensal', 'conta.cobranca.Total']])
