b7a2f359326a225083416da120b344097427f6eb
Henrique Canhadas <pedrosacanhadas@gmail.com>
2025-05-07 17:29:31 -0300

    att django fim curso admin e dados

--- Formação Django/alura-space/galeria/admin.py
+++ Formação Django/alura-space/galeria/admin.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display=("id","nome","legenda")
+    list_display=("id","nome","legenda", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_per_page = 10
+
+    list_editable = ("publicada",)
+    list_date = ("data_fotografia",)
+
 
 admin.site.register(Fotografia, ListandoFotografias)
